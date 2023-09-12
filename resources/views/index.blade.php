7ecbfcfdf69a4683baa75f0a21649f0ec153fc4a
jimmy desu <jimsyyap@gmail.com>
2023-09-12 20:50:18 +1000

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Task List in Laravel</title>
+
+    </head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Task List</h1>
+            <div>
+                @forelse ($tasks as $task)
+                    <div>
+                        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+                    </div>
+            @empty
+                <div>you have no tasks</div>
+            @endforelse
+            </div>
+        </div>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </body>
+</html>
