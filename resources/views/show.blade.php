3bc97a371824156ffe6f11788dbf5304ae2582c4
jimmy desu <jimsyyap@gmail.com>
2023-09-13 16:04:24 +1000

    aoeu

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>show blade template</title>
+@extends('layout.header')
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-</head>
-<body>
+@section('title', $task->title)
+{{-- no need @endsection as this line doesnt have html --}}
+
+@section('content')
     <div class="container">
 
         <h1>{{ $task -> title }}</h1>
@@ -19,8 +16,6 @@
         <p>{{ $task->created_at }}</p>
         <p>{{ $task->updated_at }}</p>
         </div>
+@endsection
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</body>
-</html>
+@extends('layout.footer')
