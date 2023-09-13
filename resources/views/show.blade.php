c400707aee69f64986b1f37ec312178eb70ceb5e
jimmy desu <jimsyyap@gmail.com>
2023-09-13 12:48:58 +1000

    added views layout header footer

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <title>show blade template</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
-    <h1>{{ $task -> title }}</h1>
-    <p>{{ $task->description }}</p>
+    <div class="container">
+
+        <h1>{{ $task -> title }}</h1>
+        <p>{{ $task->description }}</p>
+
+        @if ($task->long_description)
+            <p>{{ $task->long_description }}</p>
+        @endif
+
+        <p>{{ $task->created_at }}</p>
+        <p>{{ $task->updated_at }}</p>
+        </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
