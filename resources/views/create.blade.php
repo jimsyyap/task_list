e10fa2fea72b30fcdf48a049b51d428b69887408
jimmy desu <jimsyyap@gmail.com>
2023-09-20 16:07:27 +1000

    aoeu

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,28 +5,28 @@
 @section('content')
     <div class="container">
 
-      <form method="POST" action="{{ route('tasks.store') }}">
-        @csrf
-        <div>
-          <label for="title">
-            Title
-          </label>
-          <input text="text" name="title" id="title" />
-        </div>
+        <form method="POST" action="{{ route('tasks.store') }}">
+            @csrf
+            <div>
+                <label for="title">
+                    Title
+                </label>
+                <input text="text" name="title" id="title" />
+            </div>
 
-        <div>
-          <label for="description">Description</label>
-          <textarea name="description" id="description" rows="5"></textarea>
-        </div>
+            <div>
+                <label for="description">Description</label>
+                <textarea name="description" id="description" rows="5"></textarea>
+            </div>
 
-        <div>
-          <label for="long_description">Long Description</label>
-          <textarea name="long_description" id="long_description" rows="10"></textarea>
-        </div>
+            <div>
+                <label for="long_description">Long Description</label>
+                <textarea name="long_description" id="long_description" rows="10"></textarea>
+            </div>
 
-        <div>
-          <button type="submit">Add Task</button>
-        </div>
-      </form>
+            <div>
+                <button type="submit">Add Task</button>
+            </div>
+        </form>
     </div>
 @endsection
